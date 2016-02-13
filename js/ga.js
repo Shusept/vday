7897fb648926d14a2dc22f092ff077bed577b83c
Shu <shu@shushun-no-MacBook-Pro.local>
2016-02-13 12:07:58 +0800

    domain update

--- js/ga.js
+++ js/ga.js
@@ -1,16 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?43b4f6ac952a9143eb076a62359b1da6";
+  hm.src = "//hm.baidu.com/hm.js?fc55c56d80ccfc504e178950e14c38d1";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-73113177-1', 'auto');
-  ga('send', 'pageview');
